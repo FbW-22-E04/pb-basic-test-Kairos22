44ccaadae088fc7529794cb5db31cd60d654a703
Kairos22 <alandiko84@gmail.com>
2022-08-22 14:38:51 +0200

    modify

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ function isDivisible(num) {
       return result
 }
      console.log(isDivisible(1));
+     console.log(isDivisible(1000));
+     console.log(isDivisible(100));
+     
      console.log('-----------------------------------2');
 
 // 3.What's the weather?
@@ -32,7 +35,43 @@ function isRaining (result) {
     result = (isRaining(true) ? 'wet day - you need an umbrella' :  'leave your umbrella at home' );
 }
 console.log('result');
+console.log('------------------------------------------3');
 
-// DO NOT EDIT below this line - This will result in an automatic fail
-// module.exports = {nameOfCity, isDivisible, missingAngle, isRaining, geometricalSequence, multiplesOfThree, powerOf, vowelCount};
+// Loops
+// 1.Sequence
+function geometricalSequence() {
+for (let i = 0; i < 9; i++)
+n = n * n;
+    result = result + n ;
+  }
+  console.log(result);
 
+console.log('----------------------------------Loops--1.Sequence');
+// Math
+function powerOf(int) {
+  let powervalue = int ** int;
+  return console.log(powervalue);
+}
+powerOf(3);
+powerOf(4);
+console.log('-----------------------------Math');
+// Problem Solving 
+function vowelCount(str) {
+  let vowelCount = 0;
+
+  
+  let string = str.toString();
+
+  for (let i = 0; i <= string.length - 1; i++) {
+
+    if (string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u") {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
+}
+
+console.log(vowelCount("hello")); 
+console.log(vowelCount("test"));
+console.log(vowelCount("fbw"));
+console.log('----------------------Problem Solving');
